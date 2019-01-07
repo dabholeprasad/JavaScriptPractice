15d349082312f256415dbc65e9d22621df98616b
dabholeprasad <dabholeprasad9894@gmail.com>
2019-01-07 23:16:03 +0530

    Basics JS

---
+++ V05-Basic.js
@@ -0,0 +1,30 @@
+// var person =
+// {
+//     firstName:"Prasad",
+//     lastName:"Dabhole",
+//     Age:31
+// }
+//     console.log(person);
+
+function addnumber(a,b)
+{
+ return(a+b);
+}
+console.log(addnumber( 7, 3));
+////..........................
+
+function undefined()
+{
+}
+console.log(undefined());
+////...........................................
+var ananymous = function()
+{
+console.log("Hi Friends");
+}
+ananymous();
+
+setTimeout(ananymous, 5000)
+////...........................................
+
+
