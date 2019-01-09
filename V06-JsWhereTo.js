bce58d6cf7dbb5f99f8dc08f47e5b494ffc1d397
dabholeprasad <dabholeprasad9894@gmail.com>
2019-01-09 22:33:21 +0530

    JS where To

---
+++ V06-JsWhereTo.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script>
+function myFunction() 
+{
+  document.getElementById("PP").innerHTML = "PP changed.";
+}
+</script>
+</head>
+<body>
+<h1>A Web Page</h1>
+<p id="PP">A Paragraph</p>
+<button type="button" onclick="myFunction()">Try it</button>
+</body>
+</html>
